7370e099d046528685a60d008ecdd3d0452388cb
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-07 13:19:57 +1000

    Add search function

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    if params[:search] == "t"
+      @origin = Airport.find_by(location: params[:origin])
+      @destination = Airport.find_by(location: params[:destination])
+      @flights = Flight.where(origin: @origin, destination: @destination)
+    else
+      @flights = Flight.all
+    end
+    @airports = Airport.all
   end
 end
