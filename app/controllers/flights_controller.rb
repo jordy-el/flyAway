c1a2632ee3ab57f76bf07eb90627ee18fc8b14f0
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-07 13:30:27 +1000

    Improve search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,10 @@ class FlightsController < ApplicationController
     if params[:search] == "t"
       @origin = Airport.find_by(location: params[:origin])
       @destination = Airport.find_by(location: params[:destination])
-      @flights = Flight.where(origin: @origin, destination: @destination)
+      @flights = Flight.where(origin: @origin)
+      if @destination
+        @flights = @flights.reject { |flight| flight.destination != @destination }
+      end
     else
       @flights = Flight.all
     end
