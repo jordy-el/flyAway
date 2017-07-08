d74bc7c71cd3d4070016cb02e50776593fe0d558
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-08 18:04:40 +1000

    Improve index UI

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,13 +5,13 @@ class FlightsController < ApplicationController
       @destination = Airport.find_by(location: params[:destination])
       @flights = Flight.where(origin: @origin)
       if @destination
-        @flights = @flights.reject { |flight| flight.destination != @destination }
+        @flights = @flights.where(destination: @destination)
       end
     else
       @flights = Flight.all
     end
     if @flights.empty?
-      flash[:error] = "There were no results matching that search."
+      flash[:error] = "We found nothing matching your search criteria"
       redirect_to root_url
     end
     @flights = @flights.order(:departing)
