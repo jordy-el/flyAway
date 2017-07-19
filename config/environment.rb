f0156612c1f826b78b750d6d2a7a426469f78ec3
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-07-19 12:47:18 +1000

    Rework environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-<<<<<<< HEAD
 ActionMailer::Base.smtp_settings = {
   :user_name => 'NIL',
   :password => 'NIL',
@@ -11,17 +10,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-=======
-#ActionMailer::Base.smtp_settings = {
-#  :user_name => 'NIL',
-#  :password => 'NIL',
-#  :domain => 'infinite-earth-40983.herokuapp.com',
-#  :address => 'smtp.sendgrid.net',
-#  :port => 587,
-#  :authentication => :plain,
-#  :enable_starttls_auto => true
-#}
->>>>>>> 7ef6a0bff7e0485d124e5dc4a8f1ca299a758acd
 
 # Initialize the Rails application.
 Rails.application.initialize!
